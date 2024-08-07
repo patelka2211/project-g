a1cb204a458c75b35e19cd5737467372c4f5c831
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2024-08-07 17:02:29 +0530

    Project setup complete

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { open } from "@tauri-apps/api/dialog";
+    import { documentDir } from "@tauri-apps/api/path";
+  
+    let selectedFolder: Awaited<ReturnType<typeof open>> = null;
+  
+    async function folderSelector() {
+      selectedFolder = await open({
+        defaultPath: await documentDir(),
+        directory: true,
+        multiple: false,
+      });
+    }
+  </script>
+  
+  <button on:click={folderSelector}>Select folder</button>
+  
+  {#if selectedFolder !== null}
+    <br />
+  
+    Selected folder:
+    {selectedFolder}
+  {/if}
+  
+  <style>
+  </style>
+  
