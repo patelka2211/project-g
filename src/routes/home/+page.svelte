44ae3b0e7e032942d9fdf7a2da6a4059e28eccc3
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2024-08-18 14:46:04 +0530

    Started working on UI

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
-  import Button from "@/components/ui/button/button.svelte";
+  import { Button } from "@/components/ui/button";
+  import Sonner from "@/components/ui/sonner/sonner.svelte";
   import { open } from "@tauri-apps/api/dialog";
   import { documentDir } from "@tauri-apps/api/path";
+  import { toast } from "svelte-sonner";
 
   let errorMsg: undefined | string = undefined;
 
@@ -21,10 +23,31 @@
       errorMsg = "Cannot select multiple folders!";
     }
   }
+
+  $: {
+    if (errorMsg !== undefined) {
+      toast.error(errorMsg);
+      errorMsg = undefined;
+    }
+  }
 </script>
 
-<Button on:click={folderSelector}>Select folder</Button>
+<Sonner richColors expand />
 
-{#if typeof errorMsg === "string"}
-  {errorMsg}
-{/if}
+<div class="root flex flex-col items-center justify-around">
+  <div class="flex flex-col items-center">
+    <h1 class="font-semibold text-3xl mb-6">Welcome to Project G</h1>
+    <Button on:click={folderSelector} class="mb-4">Select folder</Button>
+  </div>
+</div>
+
+<style lang="scss">
+  * {
+    &:hover {
+      cursor: default;
+    }
+  }
+  .root {
+    min-height: calc(100dvh - 28px);
+  }
+</style>
