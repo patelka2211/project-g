f426d78fb3560d51b29e13a1dc61ed9b67585e40
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2024-08-19 14:39:49 +0530

    Full screen detection implemented.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,25 @@
 <script>
+  import { appWindow } from "@tauri-apps/api/window";
   import { ModeWatcher } from "mode-watcher";
+  import { onMount } from "svelte";
   import "../app.css";
+
+  let titlebarHidden = false;
+
+  async function checkFullscreen() {
+    titlebarHidden = await appWindow.isFullscreen();
+  }
+
+  onMount(() => {
+    appWindow.onResized(checkFullscreen);
+  });
 </script>
 
 <ModeWatcher />
 
-<div data-tauri-drag-region class="border-b border-accent"></div>
+{#if !titlebarHidden}
+  <div data-tauri-drag-region class="border-b border-accent"></div>
+{/if}
 
 <slot></slot>
 
