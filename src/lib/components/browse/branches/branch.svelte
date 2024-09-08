c4583f17ac19eb55ba5775f23e1ea9c0d1905175
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2024-09-08 09:41:40 +0530

    Branch name and menu partially implemented

--- src/lib/components/browse/branches/branch.svelte
+++ src/lib/components/browse/branches/branch.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import type { BranchInfo } from "@/integrated-backend/browse/branches";
   import Actions from "./actions.svelte";
+  import NameAndMenu from "./name-and-menu.svelte";
 
   export let branch: BranchInfo;
 </script>
 
 <div class="p-[6px] border rounded-[12px]">
+  <NameAndMenu {branch} />
   <Actions {branch} />
-  {branch.name}
 </div>
---
+++ src/lib/components/browse/branches/name-and-menu.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import EllipsisIcon from "@/codicons/ellipsis-icon.svelte";
+  import type { BranchInfo } from "@/integrated-backend/browse/branches";
+
+  export let branch: BranchInfo;
+</script>
+
+<div class="h-[28px] flex items-center justify-between mb-[6px]">
+  <div
+    class="w-[calc(100%-28px-6px)] h-full rounded-[6px] px-[6px] hover:bg-accent hover:cursor-pointer"
+  >
+    {branch.name}
+  </div>
+  <button
+    class="aspect-square h-full border rounded-[6px] hover:bg-accent hover:shadow-inner hover:border-accent"
+  >
+    <EllipsisIcon class="aspect-square h-3/4 m-auto" />
+  </button>
+</div>
