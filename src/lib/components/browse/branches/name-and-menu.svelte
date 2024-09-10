5325f23fb402dfa32544373ff999445b8d2d91df
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2024-09-10 09:15:53 +0530

    Implemented basic branch info modal

--- src/lib/components/browse/branches/name-and-menu.svelte
+++ src/lib/components/browse/branches/name-and-menu.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
   import EllipsisIcon from "@/codicons/ellipsis-icon.svelte";
+  import { Modal } from "@/components/modal";
   import type { BranchInfo } from "@/integrated-backend/browse/branches";
 
   export let branch: BranchInfo;
+
+  let showBranchModal = false;
 </script>
 
+<Modal bind:showModal={showBranchModal}>
+  {branch.name}
+  <br />
+  {branch.upstream}
+</Modal>
+
 <div class="h-[28px] flex items-center justify-between mb-[6px]">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
   <div
     class="w-[calc(100%-28px-6px)] h-full rounded-[6px] px-[6px] hover:bg-accent hover:cursor-pointer"
+    on:click={() => (showBranchModal = true)}
   >
     {branch.name}
   </div>
