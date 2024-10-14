ca895acb08d18a54655030b60f328c84fd1406f3
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2024-10-14 13:36:46 +0530

    Started migrating Svelte v4 to v5.

--- src/lib/codicons/add-icon.svelte
+++ src/lib/codicons/add-icon.svelte
@@ -1,3 +1,11 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...$$props}>
+<script lang="ts">
+	interface Props {
+		[key: string]: any;
+	}
+
+	let { ...props }: Props = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
 	<path fill="currentColor" d="M14 7v1H8v6H7V8H1V7h6V1h1v6z" />
 </svg>
